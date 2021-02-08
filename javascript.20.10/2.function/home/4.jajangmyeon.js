32b3a2abbd4d87ecf5f4286c2a8a27a80efecd48
a <a@a.com>
2021-02-08 17:51:08 +0900

    a

--- javascript.20.10/2.function/home/4.jajangmyeon.js
+++ javascript.20.10/2.function/home/4.jajangmyeon.js
@@ -2,12 +2,12 @@
 과제3:
 2.callback.js 의 짜장면 이야기를, => function 으로 refactoring 한다.
 */
-let shef1 = (food) => `sweet ${food}`;
+let chef1 = (food) => `sweet ${food}`;
 
-let shef2 = food => `hot ${food}`;
+let chef2 = food => `hot ${food}`;
 
-let waiter = (food, shef) => shef(food);
+let waiter = (food, chef) => chef(food);
 
-console.log(waiter('jajangmyeon', shef1), // sweet jajangmyeon
-	    	waiter('jajangmyeon', shef2), // hot jajangmyeon
-	    	waiter('champon', shef2));    // hot champon
+console.log(waiter('jajangmyeon', chef1), // sweet jajangmyeon
+	    	waiter('jajangmyeon', chef2), // hot jajangmyeon
+	    	waiter('champon', chef2));    // hot champon
