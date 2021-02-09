8399e6fe1ab3374148d7545cade10a42e887434b
a <a@a.com>
2021-02-09 16:39:03 +0900

    a

--- javascript.20.10/5.error/1.try.js
+++ javascript.20.10/5.error/1.try.js
@@ -1,22 +1,22 @@
-let json = '{"userName":"john", "age":30}';
+let json = '{"name" : "john", "age" : 30}';
 
 let user = JSON.parse(json);
 
-console.log(typeof user);   // object
-console.log(user.userName); // john
-console.log(user.age);      // 30
+console.log(typeof user); // object
+
+console.log(user.name);   // john
+console.log(user.age);    // 30
 
 
-/** try catch **/
-// runtime error = exception
+/* try */
 json = '{age : 30}';
 
-// JSON.parse(json); // SyntaxError: Unexpected token b in JSON at position 1
+// JSON.parse(json); // SyntaxError: Unexpected token a in JSON at position 1
 
 try {
 	JSON.parse(json);
 } catch {
-	console.log('error'); // error 
+	console.log('ERROR');
 }
 
 try {
@@ -24,5 +24,10 @@ try {
 } catch(e) {
 	console.log(e.name, '\n');     // SyntaxError 
 	console.log(e.message, '\n');  // Unexpected token a in JSON at position 1 
-	console.log(e.stack);          // SyntaxError: Unexpected ...    at JSON.parse (<a ...
+	console.log(e.stack);
+	/*
+	 	SyntaxError: Unexpected token a in JSON at position 1
+    	at JSON.parse (<anonymous>)
+    	at Object.<anonymous> (C:\DEV\wor ...
+	 */
 }
